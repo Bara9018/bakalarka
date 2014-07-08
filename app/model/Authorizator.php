a2ce915f96b6dca781e5c7dba0650d12536d0ed3
Bara9018 <stupakova.barbora@gmail.com>
2014-07-08 10:37:22 +0200

    role

--- app/model/Authorizator.php
+++ app/model/Authorizator.php
@@ -1,8 +1,18 @@
 <?php
 
+use Nette\Security\Permission;
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
+class Authorizator extends Permission{
+    public function __construct() {
+	$this->addRole('guest');
+	$this->addRole('employee', 'guest');
+	$this->addRole('expedient','employee');
+	$this->addRole('odoberatel', 'employee');
+	$this->addRole('admin', 'expedient');
+    }
+}
